d710de485f64d904a95ac6dbebbb7754a2385df0
wiz-zy <81806709+wiz-zy@users.noreply.github.com>
2021-04-02 04:22:44 -0700

    Update Wiz Bot.py

--- Wiz Bot.py
+++ Wiz Bot.py
@@ -4,6 +4,14 @@ import discord
 import asyncio
 import random
 import logging
+import os.path
+import nekos
+import hentai
+from hentai import Hentai
+import json
+from hentai import Format
+from urllib.parse import urlparse
+from os.path import splitext
 import random
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -50,6 +58,363 @@ async def bllist(ctx):
         )
         embed.set_footer(text="coded by wizard#5236")
         await ctx.send(embed=embed)
+
+
+@client.command()
+async def keywords(ctx):
+    possible = [
+        "feet",
+        "yuri",
+        "trap",
+        "futanari",
+        "hololewd",
+        "lewdkemo",
+        "solog",
+        "feetg",
+        "cum",
+        "erokemo",
+        "les",
+        "wallpaper",
+        "lewdk",
+        "ngif",
+        "tickle",
+        "lewd",
+        "feed",
+        "gecg",
+        "eroyuri",
+        "eron",
+        "cum_jpg",
+        "bj",
+        "nsfw_neko_gif",
+        "solo",
+        "kemonomimi",
+        "nsfw_avatar",
+        "gasm",
+        "poke",
+        "anal",
+        "slap",
+        "hentai",
+        "avatar",
+        "erofeet",
+        "holo",
+        "keta",
+        "blowjob",
+        "pussy",
+        "tits",
+        "holoero",
+        "lizard",
+        "pussy_jpg",
+        "pwankg",
+        "classic",
+        "kuni",
+        "waifu",
+        "pat",
+        "8ball",
+        "kiss",
+        "femdom",
+        "neko",
+        "spank",
+        "cuddle",
+        "erok",
+        "fox_girl",
+        "boobs",
+        "random_hentai_gif",
+        "smallboobs",
+        "hug",
+        "ero",
+        "smug",
+        "goose",
+        "baka",
+        "woof",
+    ]
+    embed = discord.Embed(title=" ", color=0xFF0000)
+    embed.set_author(name=" ")
+    embed.add_field(name="Keywords", value=f"```{possible}```", inline=False)
+    embed.set_footer(text="powered by nekos.life")
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def ping(ctx):
+    now = datetime.datetime.now()
+    embed = discord.Embed(colour=discord.Colour.red())
+    embed.set_author(name="Pong!")
+    embed.set_footer(text=f"{round(client.latency * 1000)}ms")
+    embed.timestamp = datetime.datetime.now()
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def rantai(ctx):
+    pictures = [
+        "feet",
+        "yuri",
+        "trap",
+        "futanari",
+        "hololewd",
+        "lewdkemo",
+        "erokemo",
+        "les",
+        "lewdk",
+        "lewd",
+        "eroyuri",
+        "eron",
+        "cum_jpg",
+        "bj",
+        "solo",
+        "kemonomimi",
+        "nsfw_avatar",
+        "erofeet",
+        "holo",
+        "keta",
+        "blowjob",
+        "tits",
+        "holoero",
+        "pussy_jpg",
+        "kuni",
+        "femdom",
+        "neko",
+        "erok",
+        "boobs",
+        "smallboobs",
+        "ero",
+    ]
+    embed = discord.Embed(colour=discord.Colour.red())
+
+    randomChoice = random.choice(pictures)
+    embed.set_author(name=f'Sent an random NSFW picture with the tag "{randomChoice}"!')
+    embed.set_image(url=nekos.img(randomChoice))
+    embed.timestamp = datetime.datetime.now()
+    embed.set_footer(text="Powered by nekos.life!")
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def search(ctx, word):
+    possible = [
+        "feet",
+        "yuri",
+        "trap",
+        "futanari",
+        "hololewd",
+        "lewdkemo",
+        "solog",
+        "feetg",
+        "cum",
+        "erokemo",
+        "les",
+        "wallpaper",
+        "lewdk",
+        "ngif",
+        "tickle",
+        "lewd",
+        "feed",
+        "gecg",
+        "eroyuri",
+        "eron",
+        "cum_jpg",
+        "bj",
+        "nsfw_neko_gif",
+        "solo",
+        "kemonomimi",
+        "nsfw_avatar",
+        "gasm",
+        "poke",
+        "anal",
+        "slap",
+        "hentai",
+        "avatar",
+        "erofeet",
+        "holo",
+        "keta",
+        "blowjob",
+        "pussy",
+        "tits",
+        "holoero",
+        "lizard",
+        "pussy_jpg",
+        "pwankg",
+        "classic",
+        "kuni",
+        "waifu",
+        "pat",
+        "8ball",
+        "kiss",
+        "femdom",
+        "neko",
+        "spank",
+        "cuddle",
+        "erok",
+        "fox_girl",
+        "boobs",
+        "random_hentai_gif",
+        "smallboobs",
+        "hug",
+        "ero",
+        "smug",
+        "goose",
+        "baka",
+        "woof",
+    ]
+
+    if word in possible:
+        embed = discord.Embed(colour=discord.Colour.red())
+        embed.set_author(name=f'Sent with the tag: "{word}"!')
+        embed.set_image(url=nekos.img(word))
+        embed.timestamp = datetime.datetime.now()
+        embed.set_footer(text="Powered by nekos.life!")
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("Invalid keyword.")
+
+
+@client.command()
+async def gentai(ctx):
+    pictures = ["random_hentai_gif", "solog", "feetg", "cum", "pussy"]
+
+    embed = discord.Embed(colour=discord.Colour.red())
+
+    randomChoice = random.choice(pictures)
+    embed.set_author(name=f'Sent with the tag: "{randomChoice}"!')
+    embed.set_image(url=nekos.img(randomChoice))
+    embed.timestamp = datetime.datetime.now()
+    embed.set_footer(text="Powered by nekos.life!")
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def dentai(ctx, number):
+    cur_page = 1
+    try:
+        doujin = Hentai(number)
+    except HTTPError:
+        await ctx.send("You're stupid as fuck")
+    pages = doujin.num_pages
+    pages2 = doujin.num_pages - 1
+    member = ctx.author
+    embed = discord.Embed(colour=discord.Colour.red())
+    embed2 = discord.Embed(colour=discord.Colour.red())
+    embed.set_author(name="Would you like to read this Doujinshii?")
+    embed.add_field(
+        name=f"Doujin:", value=f"{doujin.title(Format.Pretty)}", inline=False
+    )
+    embed.add_field(name="Tags:", value=[tag.name for tag in doujin.tag], inline=False)
+    embed.set_image(url=doujin.image_urls[0])
+    message = await ctx.send(embed=embed)
+    await message.add_reaction("✅")
+    await message.add_reaction("🚫")
+
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) in ["✅", "🚫"]
+
+    try:
+        reaction, user = await client.wait_for(
+            "reaction_add", timeout=60.0, check=check
+        )
+    except asyncio.TimeoutError:
+        await member.send("You didn't respond.")
+        await message.delete()
+    else:
+        if reaction.emoji == "🚫":
+            await ctx.send("Ok.")
+            await message.delete()
+            await message.clear_reactions()
+        if reaction.emoji == "✅":
+            await message.delete()
+            embed2.set_author(
+                name=f"Doujin: {doujin.title(Format.Pretty)} [Page: {cur_page}/{pages}]"
+            )
+            embed2.add_field(
+                name="Tags:", value=[tag.name for tag in doujin.tag], inline=False
+            )
+            embed2.set_image(url=doujin.image_urls[0])
+            embed2.timestamp = datetime.datetime.now()
+            embed2.set_footer(text="Powered by NHentai")
+            message = await ctx.send(embed=embed2)
+            await message.add_reaction("◀")
+            await message.add_reaction("▶")
+            await message.add_reaction("✅")
+
+            def check2(reaction, user):
+                return user == ctx.author and str(reaction.emoji) in ["◀", "▶", "✅"]
+
+            while True:
+                try:
+                    reaction, user = await client.wait_for(
+                        "reaction_add", timeout=60.0, check=check2
+                    )
+                except asyncio.TimeoutError:
+                    await member.send(
+                        "You didn't read the next page of your doujinshii within 1 minute! Deleted your doujin."
+                    )
+                    await message.delete()
+                    break
+                else:
+                    if reaction.emoji == "▶" and cur_page != pages:
+                        cur_page += 1
+
+                        embed3 = discord.Embed(colour=discord.Colour.red())
+
+                        embed3.set_author(
+                            name=f"Doujin: {doujin.title(Format.Pretty)} [Page: {cur_page}/{pages}]"
+                        )
+                        embed3.add_field(
+                            name="Tags:",
+                            value=[tag.name for tag in doujin.tag],
+                            inline=False,
+                        )
+                        embed3.set_image(url=doujin.image_urls[cur_page - 1])
+                        embed3.timestamp = datetime.datetime.now()
+                        embed3.set_footer(text="Powered by NHentai!")
+                        await message.edit(embed=embed3)
+                        await message.remove_reaction(reaction, user)
+                    if reaction.emoji == "✅":
+                        embed5 = discord.Embed(colour=discord.Colour.red())
+
+                        if cur_page != pages:
+                            embed5.set_author(name=f"Doujin stopped at Page {cur_page}")
+                            embed5.add_field(
+                                name="Tags:",
+                                value=[tag.name for tag in doujin.tag],
+                                inline=False,
+                            )
+                            embed5.add_field(name=f"ID:", value=doujin.id, inline=False)
+                            embed5.timestamp = datetime.datetime.now()
+                            embed5.set_footer(text=f"{round(client.latency * 1000)}ms")
+                            await message.edit(embed=embed5)
+                            await message.clear_reactions()
+                            break
+                        else:
+                            embed5.set_atuhor(name=f"Finished Doujin!")
+                            embed5.add_field(
+                                name="Tags:",
+                                value=[tag.name for tag in doujin.tag],
+                                inline=False,
+                            )
+                            embed5.add_field(name=f"ID:", value=doujin.id, inline=False)
+                            embed5.timestamp = datetime.datetime.now()
+                            embed5.set_footer(text=f"{round(client.latency * 1000)}ms")
+                            await message.edit(embed=embed5)
+                            await message.clear_reactions()
+                            break
+                    elif reaction.emoji == "◀" and cur_page != 0:
+                        cur_page -= 1
+
+                        embed4 = discord.Embed(colour=discord.Colour.red())
+
+                        embed4.set_author(
+                            name=f"Doujin: {doujin.title(Format.Pretty)} [Page: {cur_page}/{pages}]"
+                        )
+                        embed4.add_field(
+                            name="Tags:",
+                            value=[tag.name for tag in doujin.tag],
+                            inline=False,
+                        )
+                        embed4.set_image(url=doujin.image_urls[cur_page - 1])
+                        embed4.timestamp = datetime.datetime.now()
+                        embed4.set_footer(text="Powered by NHentai!")
+                        await message.edit(embed=embed4)
+                        await message.remove_reaction(reaction, user)
 
 
 @commands.cooldown(1, 3, commands.BucketType.user)
@@ -169,4 +534,4 @@ async def on_command_error(ctx, error):
         await ctx.send(embed=embed)
 
 
-client.run("")
+client.run("ODE2MDg1NzM4MjU2MjAzNzk2.YD11Eg.U-vfWCo5XIQYPB4H8PN6YmN3KR0")
